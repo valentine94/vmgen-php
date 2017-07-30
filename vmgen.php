42fbefe4dfa4a8b0956d7d76b44c01a4360957f6
Valentine94 <mve@drupalway.net>
2017-07-30 23:48:14 +0300

    Update main vmgen code

--- vmgen.php
+++ vmgen.php
@@ -4,18 +4,23 @@
  * Main vmgen executable file.
  */
 
+require __DIR__ . '/tools.php';
+
 if (!empty($argv)) {
   $arguments    = parse_arguments_into_assoc_array($argv);
   $current_user = get_current_user();
   $platform     = (stripos(PHP_OS, 'darwin') === 0) ? 'Mac' : 'Linux';
   $projects_dir = ($platform === 'Mac' ? '/Users/' : '/home/') . $current_user . '/projects';
   // Ensure we have arguments.
-  if (!empty($arguments)) {
-    // Prepare Drupal VM.
-    prepare_drupal_vm();
-    // Prepare project directories structure.
-    prepare_directories($projects_dir, $arguments['project_name']);
-  }
+  // Prepare Drupal VM.
+  prepare_drupal_vm();
+  // Prepare project directories structure.
+  prepare_directories($projects_dir, $arguments['project_name']);
+  // Preparing drupal vm for the project's directory.
+  unzip_file('/tmp/drupal_vm.zip', $projects_dir . '/' . $arguments['project_name']);
+  rmove($projects_dir . '/' . $arguments['project_name'] . '/drupal-vm-4.6.0', $projects_dir . '/' . $arguments['project_name'] . '/vm');
+  chmod_r($projects_dir . '/' . $arguments['project_name']);
+  // @TODO: Create config.yml for the project.
 }
 
 /**
@@ -32,12 +37,15 @@ function prepare_directories($projects_dir, $project_name) {
   }
   if (!is_dir($projects_dir . DIRECTORY_SEPARATOR . $project_name)) {
     mkdir($projects_dir . DIRECTORY_SEPARATOR . $project_name);
+    chmod($projects_dir . DIRECTORY_SEPARATOR . $project_name, 0777);
   }
   if (!is_dir($projects_dir . DIRECTORY_SEPARATOR . $project_name . DIRECTORY_SEPARATOR . 'docroot')) {
     mkdir($projects_dir . DIRECTORY_SEPARATOR . $project_name . DIRECTORY_SEPARATOR . 'docroot');
+    chmod($projects_dir . DIRECTORY_SEPARATOR . $project_name . DIRECTORY_SEPARATOR . 'docroot', 0777);
   }
   if (!is_dir($projects_dir . DIRECTORY_SEPARATOR . $project_name . DIRECTORY_SEPARATOR . 'vm')) {
     mkdir($projects_dir . DIRECTORY_SEPARATOR . $project_name . DIRECTORY_SEPARATOR . 'vm');
+    chmod($projects_dir . DIRECTORY_SEPARATOR . $project_name . DIRECTORY_SEPARATOR . 'vm', 0777);
   }
 }
 
@@ -46,7 +54,7 @@ function prepare_directories($projects_dir, $project_name) {
  */
 function prepare_drupal_vm() {
   // Ensure we don't have a downloaded drupal_vm, to prevent doing it twice.
-  if (!is_dir('/tmp/drupal_vm.zip')) {
+  if (!is_file('/tmp/drupal_vm.zip')) {
     // Download drupal_vm to the temp directory.
     file_put_contents(
       '/tmp/drupal_vm.zip',
@@ -56,42 +64,7 @@ function prepare_drupal_vm() {
     if (!file_exists('/tmp/drupal_vm.zip')) {
       die('Drupal VM does not been downloaded');
     }
-    // Unzip the downloaded archive.
-    if (unzip_file('/tmp/drupal_vm.zip', '/tmp/drupal_vm')) {
-      echo 'Drupal VM zip archive extracted successfully';
-    }
-    else {
-      die('Drupal VM zip archive extraction failed');
-    }
-    unlink('/tmp/drupal_vm.zip');
   }
-}
-
-/**
- * Unzip the file.
- *
- * @param string $file
- *   Filename.
- * @param string $destination
- *   Destination directory.
- *
- * @return bool
- *   TRUE - if file has been unzipped, otherwise FALSE.
- */
-function unzip_file($file, $destination) {
-  // Create object.
-  $zip = new ZipArchive;
-  // Open archive.
-  if ($zip->open($file) !== TRUE) {
-    return FALSE;
-  }
-  // Check if destination directory exist, otherwise create it.
-  if (!is_dir($destination)) mkdir($destination);
-  // Extract contents to destination directory.
-  $zip->extractTo($destination);
-  // Close archive.
-  $zip->close();
-  return TRUE;
 }
 
 
